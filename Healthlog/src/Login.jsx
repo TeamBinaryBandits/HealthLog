72d5c7dfcfa9a98a3d3f70f93bfa0f5c6ba3331b
AmitabhDey-Byte <amitabhdey101@gmail.com>
2025-12-09 23:57:37 +0530

    bug fixed

--- Healthlog/src/Login.jsx
+++ Healthlog/src/Login.jsx
@@ -2,23 +2,35 @@ import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 
-function Login({setPage}) {
+export default function Login({ setPage }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLogin = () => {
     signInWithEmailAndPassword(auth, email, password)
-      .then(user => console.log("Logged in:", user))
-      .catch(err => console.log(err));
+      .then((user) => {
+        console.log("Logged in:", user);
+        setPage("dashboard"); // redirect after login
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
     <div>
-      <input onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-      <input onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+      <input
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+        type="email"
+      />
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        type="password"
+      />
       <button onClick={handleLogin}>Login</button>
+
+      <br />
+      <button onClick={() => setPage("signup")}>Go to Signup</button>
     </div>
   );
 }
-
-export default Login()
--- Healthlog/src/Signup.jsx
+++ Healthlog/src/Signup.jsx
@@ -2,23 +2,35 @@ import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 
-function Signup({setPage}) {
+export default function Signup({ setPage }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSignup = () => {
     createUserWithEmailAndPassword(auth, email, password)
-      .then(user => console.log("Signed up:", user))
-      .catch(err => console.log(err));
+      .then((user) => {
+        console.log("Signed up:", user);
+        setPage("dashboard"); // redirect after signup
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
     <div>
-      <input onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-      <input onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+      <input
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+        type="email"
+      />
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        type="password"
+      />
       <button onClick={handleSignup}>Signup</button>
+
+      <br />
+      <button onClick={() => setPage("login")}>Already have an account?</button>
     </div>
   );
 }
-
-export default Signup()
