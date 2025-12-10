49ea0e036c7424c85c2336b93e476877a3bc713a
AmitabhDey-Byte <amitabhdey101@gmail.com>
2025-12-11 02:27:29 +0530

    Changes in insight

--- Healthlog/src/InsightsPage.jsx
+++ Healthlog/src/InsightsPage.jsx
@@ -1,29 +1,40 @@
 import './InsightsPage.css';
-import React, {useState} from "react";
+import {useEffect, useState} from "react";
+import {collection, getDocs} from "firebase/firestore";
+import {db} from "../firebase.js"
 export default function InsightsPage()
 {
-const [healthdata, sethealthdata] = useState(null);
-const [insight, setinsights] = useState([]);
-const mockData = {
-    mood: [3, 4, 5, 2, 3, 4, 2],
-    sleephours: [6, 7.5, 5, 7, 8, 6, 4],
-    steps: [4000, 5000, 8000, 7500, 3000, 8000, 4600]
-};
-const getAvg = (arr)=>
-{
+const [logs, setlogs] = useState([]);
 
-return(arr.reduce((a,b) => a + b, 0)/arr.length).toFixed(1);
-}
-const getTrend =(e) =>
-{
+useEffect(()=> {
+    const loadData = async () =>
+        {
+            const snap = await getDocs(collection(db, "healthlogs"));
+            const arr =[];
 
-}
-const getSleepquality = (e) =>
-{
+          snap.forEach((doc)=> 
+        {
+            arr.push({
+                day: new Date(doc.Date().uploadAt?.seconds * 1000)
+                .toLocaleDateString("en-us", {weekday:"short"}),
+                value: 1
+            });
+        });
+        setlogs(arr)
+    };
+    loadData();
 
-}
-const getInsights = (data) =>
-{
-    
-}
+   }, []);
+
+  return (
+    <div className="container">
+      <h2>Insights</h2>
+
+
+      <div className="ai">
+        <h4>AI Suggestion</h4>
+        <p>Upload your daily logs consistently to maintain your health record!</p>
+      </div>
+      </div>
+  );
 }
