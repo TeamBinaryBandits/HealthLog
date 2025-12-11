f4300bd0bdfb26bc58db59d2cc192922dc326d68
AmitabhDey-Byte <amitabhdey101@gmail.com>
2025-12-11 11:01:06 +0530

    dashboard is a bit responsive now

--- Healthlog/src/Dashboard.jsx
+++ Healthlog/src/Dashboard.jsx
@@ -1,65 +1,47 @@
-import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "./firebase";
 import "./Dashboard.css";
-export default function Dashboard({setPage}) {
-  const [Ai, setAi] = useState();
-  const [srch, setsrch] = useState();
-  const [fone, setfone] = useState();
-  const [ftwo, setftwo] = useState();
-  const [fthree, setfthree] = useState();
-  function Aibutton(e)
 
-  {
+export default function Dashboard() {
+  const navigate = useNavigate();
 
-  }
-  function Search(e)
-  {
+  const handleLogout = () => {
+    auth.signOut();
+    navigate("/Login");
+  };
 
-  }
-  function SleepTracker(e)
-  {
-
-  }
-  function MoodTracker(e)
-  {
-
-  }
-  function StepCount(e)
-  {
-    
-  }
   return (
-    <div className="dashboard">
-      <div className="Welcome">
-        <h1>Welcome Amitabh Dey</h1>
-        <input
-          type="text"
-          className="Search-Bar"
-          placeholder="Search previous history"
-        />
-        <button className="AI Insights" onClick ={Aibutton}>AI Insights</button>
-      </div>
-      <div className = "Summary">
-        <span>
-          <p>AI Health Summary</p>
-          <p>Powered by Gemini</p>
-          <p>Based on recent logs </p>
-        </span>
-      </div>
-      <div className = "Features">
-      <div className = "f1">
-        <p>Sleep Tracker
-          </p>
-          <span></span></div>
-      <div className = "f2"><p>Moods</p>
-      <span>
-        </span></div>
-      <div className="f3"><p>Steps</p>
-      <span>
-        </span></div>
+    <div className="container">
+      <header className="header">
+        <h2>HealthLog Dashboard</h2>
+        <button className="logout" onClick={handleLogout}>Logout</button>
+      </header>
+
+      <div className="grid">
+        <div
+          className="upload"
+          onClick={() => navigate("/UploadPage")}
+        >
+          <h3>Upload Report</h3>
+          <p>Upload medical reports to analyze</p>
         </div>
+
+        <div
+          className="insight"
+          onClick={() => navigate("/InsightsPage")}
+        >
+          <h3>View Insights</h3>
+          <p>Check trends, statistics, and predictions</p>
+        </div>
+
+        <div className="Profile">
+          <h3>Your Profile</h3>
+          <p>Manage your basic health details</p>
+        </div>
+      </div>
     </div>
-    
   );
 }
+
 
 
